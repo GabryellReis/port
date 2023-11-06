f960e14a88e379de113d7dcf58a9e602f70a07da
GabryellReis <gabryellreis32@gmail.com>
2023-11-06 14:26:53 -0300

    vaaai

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <Router basename={process.env.URL}>
     <App />
-  </React.StrictMode>
+  </Router>
 );
-
-reportWebVitals();
