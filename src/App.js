9eb4b5b1af1af371dbad177f724a6b5aa59f9aa8
GabryellReis <95028659+GabryellReis@users.noreply.github.com>
2023-11-06 13:52:34 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-    </Routes>
+    <Home />
   );
 }
